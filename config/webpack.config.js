e0a91d29477164b736e0d1d104aeebf363a07b0b
Ryan Cooney <rcooney@users.noreply.github.com>
2022-02-02 20:05:03 -0800

    fix: make @arcgis package external (#15)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
     externals: [
         /^dojo\/.+$/,
         /^esri\/.+$/,
+        /^@arcgis\/.+$/,
         /^@geocortex\/.+$/,
         /^@vertigis\/.+$/,
         "react",
